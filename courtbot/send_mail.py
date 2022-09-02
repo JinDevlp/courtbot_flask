3bd3a35944187bac86883b0f5b54c4f8280596af
Jin Choi <91814116+JinDevlp@users.noreply.github.com>
2022-09-02 11:46:42 -0500

    Update send_mail.py

--- courtbot/send_mail.py
+++ courtbot/send_mail.py
@@ -6,12 +6,12 @@ from email.mime.text import MIMEText
 def send_mail(case_number, year_filed, county, phone_number, additional_phone_number, comments):
     port = 2525
     smtp_server = 'smtp.mailtrap.io'
-    login = 'fc0dae1372b656'
-    password = 'bcd6f5838e6b17'
+    login = '###'
+    password = '###'
     message = f"< h3 > New Case Submitted < /h3 ><ul ><li > case_number: {case_number} </li><li > year_filed: {year_filed} </li><li > county: {county} </li><li > phone_number: {phone_number} </li><li > additional_phone_number: {additional_phone_number} </li><li > comments: {comments} </li></ul>"
 
-    sender_email = 'jinhunchoi88@gmail.com'
-    receiver_email = 'jinhunchoi88@gmail.com'
+    sender_email = '###'
+    receiver_email = '###'
     msg = MIMEText(message, 'html')
     msg['Subject'] = 'New Case Submission'
     msg['From'] = sender_email
